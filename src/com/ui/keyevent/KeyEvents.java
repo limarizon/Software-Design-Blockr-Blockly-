be17e3094c29e02d5f0d80b433799f09c1e851c5
simon <simon.vancampenhout@gmail.com>
2020-03-25 21:43:03 +0100

    resolve bugs in logic

---
+++ src/com/ui/keyevent/KeyEvents.java
@@ -0,0 +1,35 @@
+package com.ui.keyevent;
+
+import an.awesome.pipelinr.Pipeline;
+import com.blockr.handlers.blockprogram.getblockprogram.GetBlockProgram;
+import com.blockr.handlers.world.GetWorld;
+import com.blockr.ui.components.programblocks.ProgramArea;
+import com.ui.ViewContext;
+
+import java.awt.event.KeyEvent;
+
+public final class KeyEvents {
+    private static String msg;
+    private KeyEvents() {}
+    public static void handleKeys(int id, int keyCode, char keyChar, ViewContext view, Pipeline mediator) {
+        switch(id) {
+            case KeyEvent.KEY_PRESSED:
+                if(KeyEvent.VK_F5 ==keyCode){
+                    var current = ProgramArea.parent.getActive();
+                    current.setHighlight();
+                    ProgramArea.parent.excecuteProgram();
+                    view.repaint();
+                }
+                if(KeyEvent.VK_ESCAPE ==keyCode) {
+                    ProgramArea.parent.getHighlightedBlock().resetHighlight();
+                    mediator.send(new GetWorld()).reset();
+                    mediator.send(new GetBlockProgram()).reset();
+                    view.repaint();
+                }
+                break;
+            case KeyEvent.KEY_TYPED:
+                break;
+
+        }
+    }
+}
