0187dc941b9be7c7f56511d215f716e776298ae4
limarizon <haloprox228@gmail.com>
2020-04-02 21:06:27 +0200

    Introduced UiMediator and refactored event handlers

--- blockr-application/src/com/ui/UiMediator.java
+++ blockr-application/src/com/ui/UiMediator.java
@@ -3,30 +3,28 @@ package com.ui;
 import an.awesome.pipelinr.Pipeline;
 import an.awesome.pipelinr.Pipelinr;
 import com.blockr.domain.State;
+import com.ui.event.DraggingStartedHandler;
+import com.ui.event.DraggingStoppedHandler;
+import com.ui.event.UiEvent;
+import com.ui.presenter.ProgramCreator;
 
 import java.util.stream.Stream;
 
 public class UiMediator {
 
     private Pipeline pipelinr = new Pipelinr();
+    private ProgramCreator programCreator = new ProgramCreator();
 
     public UiMediator(State state){
-                    /*
-        new Pipelinr()
+        pipelinr = new Pipelinr()
                 .with(() -> Stream.of(
+                    new DraggingStartedHandler(programCreator),
+                    new DraggingStoppedHandler(programCreator)
 
-                        new SetPaletteSelectionHandler(state)
-                    , new GetPaletteSelectionHandler(state)
-                    , new GetRootBlockHandler(state)
-                    , new AddBlockHandler(state)
-                    , new GetMouseRecordHandler(state)
-                    , new SetRecordMouseHandler(state)
-                    , new InsertBlockInProgramHandler(state)
-                    , new ResetUIStateHandler(state)
-                    , new ExecuteProgramHandler(state)
-                    , new GetBlockProgramHandler(state))
                 ));
-                     */
     }
 
+    public void send(UiEvent uiEvent) {
+        pipelinr.send(uiEvent);
+    }
 }
